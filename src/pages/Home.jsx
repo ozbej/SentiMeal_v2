9e30b6cbc30cc4b44a93121fe33b3c50f3a9cd81
Anukun Thurner <anukun.thurner@student.tugraz.at>
2022-12-27 11:18:38 +0100

    map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import {React, useState, useEffect} from 'react';
 import { Col, Row } from 'antd';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import Map from "../components/Map"
+import Map from "../components/Map";
+import { useLoadScript } from "@react-google-maps/api";
 
 function Home() {
 
     const render = (status) => {
         return <h1>{status}</h1>;
     };
-
+/*
     const [restaurants, setRestaurants] = useState([]);
 
     useEffect(()=>{
@@ -33,16 +34,20 @@ function Home() {
             console.log(restaurantArr)
         })
         .catch(error => console.log(error))
-      },[])
-    
+      },[]) */
+    const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+    });
+    if (!isLoaded) return <div>Loading...</div>;
     return (
         <>
             <Row>
                 <Col span={24}>
                     <h1 style={{textAlign:"center"}}>Home page</h1>
-                    <Wrapper apiKey={"AIzaSyAJA0KO_kEJ2RMOeWIMosfsAGS_8HpgCDg"} render={render}>
+                    
                         <Map />
-                    </Wrapper>
+                    
                 </Col>
             </Row>
         </>
