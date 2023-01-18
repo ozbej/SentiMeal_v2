196e6976915769965e6d2a8455f1a4f7c3e94100
Anukun Thurner <anukun.thurner@student.tugraz.at>
2023-01-18 02:16:23 +0100

    markers and areas

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -29,7 +29,7 @@ function Map() {
   })
 
   const [map, setMap] = React.useState(null)
-
+/*
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
@@ -37,10 +37,12 @@ function Map() {
 
     setMap(map)
   }, [])
+*/
+const onLoad = useCallback(map => (mapRef.current = map), [])
+const restaurants = useMemo(() => generateRestaurants(center), [center])
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+const onUnmount = React.useCallback(function callback(map) {setMap(null)}, [])
+
 
   return isLoaded ? (
       <GoogleMap
@@ -48,12 +50,79 @@ function Map() {
         center={center}
         zoom={15}
         onLoad={onLoad}
+        options={options}
         onUnmount={onUnmount}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
+        { /* Child components, such as markers, info windows, etc. */ 
+         (
+          <>
+            
+            <MarkerClusterer>
+              {clusterer =>
+                restaurants.map(restaurant => (
+                  <><Marker
+                    key={restaurant.lat}
+                    position={restaurant}
+                    clusterer={clusterer} />
+                    <Circle center={restaurant} radius={500} options={goodOptions} />
+                    </>
+            
+                ))
+              }
+            </MarkerClusterer>
+
+            
+            
+            
+          </>
+        )
+        }
         <></>
       </GoogleMap>
   ) : <></>
+}
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true
+}
+
+const goodOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A"
+}
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D"
+}
+const badOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252"
+}
+
+const generateRestaurants = position => {
+  const _restaurants = []
+  for (let i = 0; i < 100; i++) {
+    const direction = Math.random() < 0.5 ? -2 : 2
+    _restaurants.push({
+      lat: position.lat + Math.random() * 0.1 / direction,
+      lng: position.lng + Math.random() * 0.1 / direction
+    })
+  }
+  return _restaurants
 }
 
 export default React.memo(Map)
